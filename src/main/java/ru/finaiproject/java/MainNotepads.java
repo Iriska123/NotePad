22bdaa3b85c140e74e001fb172204778959697fa
Iriska123 <irinapetr1110@mail.ru>
2024-01-17 17:47:23 +0300

    done

--- src/main/java/ru/finaiproject/java/MainNotepads.java
+++ src/main/java/ru/finaiproject/java/MainNotepads.java
@@ -1,6 +1,5 @@
 package ru.finaiproject.java;
 
-import java.security.KeyStore;
 import java.util.*;
 
 public class MainNotepads {
@@ -8,42 +7,60 @@ public class MainNotepads {
 
         Set<NotePads> set = new HashSet<>();
 
-        set.add(new NotePads("4", "256", "Asus", "черный"));
-        set.add(new NotePads("4", "400", "Asus", "серый"));
-        set.add(new NotePads("5", "550", "НР", "белый"));
-        set.add(new NotePads("6", "500", "НР", "белый"));
+        set.add(new NotePads("8", "256", "Asus", "черный"));
+        set.add(new NotePads("8", "256", "Asus", "черный"));
+        set.add(new NotePads("16", "128", "Asus", "серый"));
+        set.add(new NotePads("64", "512", "НР", "белый"));
+        set.add(new NotePads("32", "1000", "НР", "белый"));
 
         System.out.println(checkParams(getParams(),set));
     }
-
-    public static Map<Integer,String> getParams(){
+    public static boolean stopChoice(){
+        System.out.println("Вывести результат поиска? (1 - да/ 0 - нет)" );
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите цифру, соответствующую необходимому критерию: \n" +
-                "1 - оперативная память\n2 - объем жесткого диска\n3 - производитель\n4 - цвет");
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("Введите параметр для поиска: ");
-        String param = scanner.nextLine();
+        int answ = scanner.nextInt();
+        return answ == 1;
+    }
+    public static Map<Integer,String> getParams(){
 
         Map<Integer, String> map = new HashMap<>();
-        map.put(choice, param);
+        Scanner scanner = new Scanner(System.in);
+
+        boolean b;
+        do {
+            System.out.println("Введите цифру, соответствующую необходимому критерию поиска: \n" +
+                    "1 - оперативная память\n2 - объем жесткого диска\n3 - производитель\n4 - цвет");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Введите параметр или минимальное значение указанного критерия: ");
+            String param = scanner.nextLine();
+
+            map.put(choice, param);
+            b = stopChoice();
+        } while (!b);
+
         return map;
     }
 
-    public static Set<NotePads> checkParams(Map<Integer,String> map,Set<NotePads> set) {
+    public static  Set<NotePads> checkParams(Map<Integer,String> map,Set<NotePads> set) {
         Set<NotePads> result = new HashSet<>();
+//        Map <String,NotePads> nd = new HashMap<>();
 
         for (NotePads note : set) {
             for (Map.Entry<Integer,String> maps: map.entrySet()) {
                 int k = maps.getKey();
                 switch (k) {
                     case 1:
-                        if (note.getRam().equals(maps.getValue())) {
+                        int i = Integer.parseInt(note.getRam());
+                        int i2 = Integer.parseInt(maps.getValue());
+                        if (i >= i2) {
                             result.add(note);
                         }
                     case 2:
-                        if (note.getHDCapacity().equals(maps.getValue())) {
+                        int a = Integer.parseInt(note.getHDCapacity());
+                        int a2 = Integer.parseInt(maps.getValue());
+                        if (a >= a2) {
                             result.add(note);
                         }
 
@@ -63,6 +80,7 @@ public class MainNotepads {
         if (result.isEmpty()){
             System.out.println("Неверные параметры для поиска");
         }
+
 
         return result;
 
